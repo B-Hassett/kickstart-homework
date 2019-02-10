e1c1639a0f1f575abd13ba638d08611dffa8f044
Bill Hassett <BillHassett76@gmail.com>
2019-02-10 15:26:48 -0800

    adding bash script files

---
+++ ssg-bash/build.sh
@@ -0,0 +1,5 @@
+cd Desktop/ssg-branch
+cat templates/top.html docs/about.html templates/bottom.html > index.html
+cat templates/top.html docs/education.html templates/bottom.html > education.html
+cat templates/top.html docs/experience.html templates/bottom.html > experience.html
+cat templates/top.html docs/contact.html templates/bottom.html > contact.html
